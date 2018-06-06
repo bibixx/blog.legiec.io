5afa82260c49f1a32917d4cbf073d022fe61c5d6
Bartosz Legięć <bartosz@legiec.eu>
2018-06-06 21:09:36 +0200

    Deployment fixes

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
     cd $SSH_PROJECT_FOLDER
     npm run hexo clean
     git pull
+    rm -rf node_modules
+    npm install
     npm run hexo generate
     rm -rf $SSH_COPY_TO/*
     cp -r ./public/* $SSH_COPY_TO
