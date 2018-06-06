0fc21f53e5de01be5d8d6e841908e717135ab899
Bartosz Legięć <bartosz@legiec.eu>
2018-06-06 20:39:10 +0200

    Added deployment

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+(
+  cd "$DIR/.." # Go to project dir.
+
+  ssh $SSH_USERNAME@$SSH_HOSTNAME -o StrictHostKeyChecking=no <<-EOF
+    cd $SSH_PROJECT_FOLDER
+    npm run hexo clean
+    git pull
+    npm run hexo generate
+    rm -rf $SSH_COPY_TO/*
+    cp -r ./public/* $SSH_COPY_TO
+EOF
+)
